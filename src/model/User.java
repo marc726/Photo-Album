d84cdd79c15b2ca90db7bb44362039a47f074b9a
marc726 <marcrizzolo726@gmail.com>
2023-11-01 12:50:41 -0400

    Structure changes. User implementation.

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,16 @@
-
 public class User {
     
+    private String username;
+
+    public User (String username, String password){
+        this.username = username;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
 }
