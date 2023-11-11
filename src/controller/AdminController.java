b58be81eca22ffcf35316599c57b9465ae5b2414
Sodapop909 <bhavyapatel04@gmail.com>
2023-11-11 15:14:04 -0500

    Working on the Admin Dash Panel

--- src/controller/AdminController.java
+++ src/controller/AdminController.java
@@ -1,5 +1,39 @@
+/**
+ * @author Bhavya Patel
+ * 
+ */
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
 public class AdminController {
-    
+
+    @FXML
+    public void handleCreateUserButtonAction(ActionEvent event) {
+        // Placeholder for creating a user
+        
+    }
+
+    @FXML
+    public void handleDeleteUserButtonAction(ActionEvent event) {
+        // Placeholder for deleting a user
+    }
+
+    @FXML
+    public void handleListUsersButtonAction(ActionEvent event) {
+        // Placeholder for listing users
+    }
+
+    @FXML
+    public void handleLogoutButtonAction(ActionEvent event) throws Exception {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginScene.fxml"));
+        stage.setScene(new Scene(root));
+    }
 }
--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -5,7 +5,15 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 
 /**
  * This class is the controller for the login view. It handles the login button action and initializes the view.
@@ -16,14 +24,39 @@ public class LoginController {
     @FXML
     private Label label;
 
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
     /**
      * This method handles the login button action.
      *
      * @param event The event triggered by the login button.
      */
+    
     @FXML
     public void handleLoginButtonAction(ActionEvent event) {
         // Placeholder for login logic
+        Stage stage = (Stage) usernameTextField.getScene().getWindow();
+        Parent root;
+
+        if ("admin".equals(usernameTextField.getText()) && "admin".equals(passwordTextField.getText())) {
+            try {
+                root = FXMLLoader.load(getClass().getResource("/view/AdminDashScene.fxml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        } else {
+            // Placeholder for regular login logic
+            return;
+        }
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
